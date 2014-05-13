21a277a194d496a61ec157931e6983a0808bbdfa
Onne Gorter <o.gorter@activevideo.com>
2014-05-12 23:07:31 -0500

    add single capture to regex and throw error properly

--- vm/regex.c
+++ vm/regex.c
@@ -11,18 +11,19 @@ struct tlRegex {
 static tlKind _tlRegexKind = { .name = "Regex" };
 tlKind* tlRegexKind = &_tlRegexKind;
 
-tlRegex* tlRegexNew(tlString* str, int flags) {
-    tlRegex* regex = tlAlloc(tlRegexKind, sizeof(tlRegex));
-    int r = regcomp(&regex->compiled, tlStringData(str), flags|REG_EXTENDED);
-    if (r) warning("%d", r);
-    return regex;
-}
-
 static tlHandle _Regex_new(tlArgs* args) {
     tlString* str = tlStringCast(tlArgsGet(args, 0));
     if (!str) TL_THROW("require a pattern");
     int flags = tl_int_or(tlArgsGet(args, 1), 0);
-    return tlRegexNew(str, flags);
+
+    tlRegex* regex = tlAlloc(tlRegexKind, sizeof(tlRegex));
+    int r = regcomp(&regex->compiled, tlStringData(str), flags|REG_EXTENDED);
+    if (r) {
+        char buf[1024];
+        regerror(r, &regex->compiled, buf, sizeof(buf));
+        TL_THROW("Regex: %s", buf);
+    }
+    return regex;
 }
 
 static tlHandle _regex_match(tlArgs* args) {
@@ -33,13 +34,17 @@ static tlHandle _regex_match(tlArgs* args) {
     int offset = tl_int_or(tlArgsGet(args, 1), 0);
     if (offset < 0 || offset >= tlStringSize(str)) return tlNull;
 
-    regmatch_t m[1];
-    int r = regexec(&regex->compiled, tlStringData(str) + offset, 1, m, offset?REG_NOTBOL:0);
+    regmatch_t m[3];
+    int r = regexec(&regex->compiled, tlStringData(str) + offset, 3, m, offset?REG_NOTBOL:0);
     if (r == REG_NOMATCH) return tlNull;
     if (r == REG_ESPACE) TL_THROW("oeps");
 
     int b = offset + m[0].rm_so;
     int e = offset + m[0].rm_eo;
+    if (m[1].rm_so != -1 && m[2].rm_so == -1) {
+        b = offset + m[1].rm_so;
+        e = offset + m[1].rm_eo;
+    }
     tlString* res = tlStringFromCopy(tlStringData(str) + b, e - b);
     return tlResultFrom(res, tlINT(b), tlINT(e), null);
 }
