856de1984a228acc3f08344ebee727f62860a973
onnlucky <onne@onnlucky.com>
2014-07-29 07:31:24 +0200

    add time functions, inline with time.h

--- vm/time.c
+++ vm/time.c
@@ -17,7 +17,40 @@ static tlSym _s_gmtoff;
 
 static tlObject* _timeMap;
 
-static tlHandle _Date(tlArgs* args) {
+// TODO add microseconds
+static tlHandle object_from_tm(struct tm* tm) {
+    tlObject *res = tlClone(_timeMap);
+    tlObjectSet_(res, _s_sec, tlINT(tm->tm_sec));
+    tlObjectSet_(res, _s_min, tlINT(tm->tm_min));
+    tlObjectSet_(res, _s_hour, tlINT(tm->tm_hour));
+    tlObjectSet_(res, _s_mon, tlINT(tm->tm_mon));
+    tlObjectSet_(res, _s_year, tlINT(tm->tm_year));
+    tlObjectSet_(res, _s_wday, tlINT(tm->tm_wday));
+    tlObjectSet_(res, _s_mday, tlINT(tm->tm_mday));
+    tlObjectSet_(res, _s_yday, tlINT(tm->tm_yday));
+    tlObjectSet_(res, _s_isdst, tlINT(tm->tm_isdst));
+    tlHandle zone = tm->tm_zone? tlStringFromCopy(tm->tm_zone, 0) : tlNull;
+    tlObjectSet_(res, _s_zone, zone);
+    tlObjectSet_(res, _s_gmtoff, tlINT(tm->tm_gmtoff));
+    return res;
+}
+
+static void object_to_tm(tlObject* from, struct tm* tm) {
+    tm->tm_sec = tl_int(tlObjectGet(from, _s_sec));
+    tm->tm_min = tl_int(tlObjectGet(from, _s_min));
+    tm->tm_hour = tl_int(tlObjectGet(from, _s_hour));
+    tm->tm_mon = tl_int(tlObjectGet(from, _s_mon));
+    tm->tm_year = tl_int(tlObjectGet(from, _s_year));
+    tm->tm_wday = tl_int(tlObjectGet(from, _s_wday));
+    tm->tm_mday = tl_int(tlObjectGet(from, _s_mday));
+    tm->tm_yday = tl_int(tlObjectGet(from, _s_yday));
+    tm->tm_isdst = tl_int(tlObjectGet(from, _s_isdst));
+    tlString* zone = tlStringCast(tlObjectGet(from, _s_isdst));
+    tm->tm_zone = zone? (char*)tlStringData(zone) : null;
+    tm->tm_gmtoff = tl_int(tlObjectGet(from, _s_gmtoff));
+}
+
+static tlHandle _localtime(tlArgs* args) {
     time_t time;
     if (tlArgsSize(args) > 0) {
         time = tl_double(tlArgsGet(args, 0));
@@ -28,24 +61,24 @@ static tlHandle _Date(tlArgs* args) {
     }
     struct tm tm;
     localtime_r(&time, &tm);
-
-    // TODO add tv using a float ...
-    tlObject *res = tlClone(_timeMap);
-    tlObjectSet_(res, _s_sec, tlINT(tm.tm_sec));
-    tlObjectSet_(res, _s_min, tlINT(tm.tm_min));
-    tlObjectSet_(res, _s_hour, tlINT(tm.tm_hour));
-    tlObjectSet_(res, _s_mon, tlINT(tm.tm_mon));
-    tlObjectSet_(res, _s_year, tlINT(tm.tm_year));
-    tlObjectSet_(res, _s_wday, tlINT(tm.tm_wday));
-    tlObjectSet_(res, _s_mday, tlINT(tm.tm_mday));
-    tlObjectSet_(res, _s_yday, tlINT(tm.tm_yday));
-    tlObjectSet_(res, _s_isdst, tlINT(tm.tm_isdst));
-    tlObjectSet_(res, _s_zone, tlStringFromCopy(tm.tm_zone, 0));
-    tlObjectSet_(res, _s_gmtoff, tlINT(tm.tm_gmtoff));
-    return res;
+    return object_from_tm(&tm);
 }
 
-static tlHandle _seconds(tlArgs* args) {
+static tlHandle _gmtime(tlArgs* args) {
+    time_t time;
+    if (tlArgsSize(args) > 0) {
+        time = tl_double(tlArgsGet(args, 0));
+    } else {
+        struct timeval tv;
+        gettimeofday(&tv, null);
+        time = tv.tv_sec;
+    }
+    struct tm tm;
+    gmtime_r(&time, &tm);
+    return object_from_tm(&tm);
+}
+
+static tlHandle _time(tlArgs* args) {
     if (tlArgsSize(args) == 0) {
         struct timeval tv;
         gettimeofday(&tv, null);
@@ -56,19 +89,47 @@ static tlHandle _seconds(tlArgs* args) {
     tlObject* from = tlObjectCast(tlArgsGet(args, 0));
     if (!from) TL_THROW("expect a map");
     struct tm tm;
-    tm.tm_sec = tl_int(tlObjectGet(from, _s_sec));
-    tm.tm_min = tl_int(tlObjectGet(from, _s_min));
-    tm.tm_hour = tl_int(tlObjectGet(from, _s_hour));
-    tm.tm_mon = tl_int(tlObjectGet(from, _s_mon));
-    tm.tm_year = tl_int(tlObjectGet(from, _s_year));
-    tm.tm_wday = tl_int(tlObjectGet(from, _s_wday));
-    tm.tm_mday = tl_int(tlObjectGet(from, _s_mday));
-    tm.tm_yday = tl_int(tlObjectGet(from, _s_yday));
-    tm.tm_isdst = tl_int(tlObjectGet(from, _s_isdst));
-    tlString* zone = tlStringCast(tlObjectGet(from, _s_isdst));
-    if (zone) tm.tm_zone = (char*)tlStringData(zone);
-    tm.tm_gmtoff = tl_int(tlObjectGet(from, _s_gmtoff));
+    object_to_tm(from, &tm);
     return tlFLOAT(mktime(&tm));
+}
+
+static tlHandle _strptime(tlArgs* args) {
+    tlString* format = tlStringCast(tlArgsGet(args, 0));
+    if (!format) TL_THROW("strptime requires a format string arg[1]");
+    tlString* input = tlStringCast(tlArgsGet(args, 1));
+    if (!input) TL_THROW("strptime requires a time string as arg[2]");
+
+    struct tm tm;
+    strptime(tlStringData(input), tlStringData(format), &tm);
+    return object_from_tm(&tm);
+}
+
+static tlHandle _strftime(tlArgs* args) {
+    tlString* format = tlStringCast(tlArgsGet(args, 0));
+    if (!format) TL_THROW("strftime requires a format string arg[1]");
+
+    struct tm tm;
+    tlHandle arg1 = tlArgsGet(args, 1);
+    if (arg1) {
+        if (tlNumberIs(arg1)) {
+            time_t time;
+            time = tl_double(arg1);
+            localtime_r(&time, &tm);
+        } else if (tlObjectIs(arg1)) {
+            object_to_tm(tlObjectAs(arg1), &tm);
+        } else {
+            TL_THROW("strftime arg[1] must be a timestamp or a map");
+        }
+    } else {
+        struct timeval tv;
+        gettimeofday(&tv, null);
+        time_t time = tv.tv_sec;
+        localtime_r(&time, &tm);
+    }
+
+    char buf[1024];
+    int size = strftime(buf, sizeof(buf), tlStringData(format), &tm);
+    return tlStringFromCopy(buf, size);
 }
 
 static void time_init() {
@@ -87,7 +148,10 @@ static void time_init() {
     _timeMap = tlObjectNew(keys);
     tlObjectToObject_(_timeMap);
 
-    tl_register_global("Date", tlNATIVE(_Date, "Date"));
-    tl_register_global("seconds", tlNATIVE(_seconds, "seconds"));
+    tl_register_global("time", tlNATIVE(_time, "time"));
+    tl_register_global("localtime", tlNATIVE(_localtime, "localtime"));
+    tl_register_global("gmtime", tlNATIVE(_gmtime, "gmtime"));
+    tl_register_global("strptime", tlNATIVE(_strptime, "strptime"));
+    tl_register_global("strftime", tlNATIVE(_strftime, "strftime"));
 }
 
