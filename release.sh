172493dfcb30d11118e6bcf6161fd950e9eb1c63
Onne Gorter <onne@onnlucky.com>
2015-05-29 18:14:40 +0200

    updating release.sh

--- release.sh
+++ release.sh
@@ -6,21 +6,25 @@ VERSION=$1
 git status | grep "Changes not staged for commit" && echo "require a clean working tree" && exit 1
 
 # ensure pregenerated boot files are up to date
+echo "--- make pregen"
 make pregen
 git add boot/pregen/*
 
+echo "--- git add config.h"
 # update version in config.h
-sed -i -e "s/define TL_VERSION_RAW .*/define TL_VERSION_RAW $VERSION/g" config.h
+sed -i .o -e "s/define TL_VERSION_RAW .*/define TL_VERSION_RAW $VERSION/g" config.h
 git add config.h
 
+echo "--- git commit and tag"
 # create commit and tag
 git commit -m"releasing $VERSION"
 git tag -a $VERSION -m"$VERSION"
 
+echo "--- make test"
 # make sure it works
 make clean
 make BUILD=release test
 
-echo "all good, pushing"
+echo "--- all good, pushing"
 git push origin $VERSION HEAD
 
